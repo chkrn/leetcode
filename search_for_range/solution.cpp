0ed7742b4d2d66d391a4c790e9abb3905a052c1a
chkrn <chkrn@bk.ru>
2020-04-22 18:49:26 +0300

    Search for a Range

---
+++ search_for_range/solution.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/802/
+
+class Solution {
+	int t;
+	int left = -1, right = -1;
+
+	void enlageToBoundries(vector<int>& nums) {
+		while(left) {
+			if(nums[left - 1] != t)
+				break;
+			
+			left--;
+		}
+
+		int last = nums.size() - 1;
+		while(right != last) {
+			if(nums[right + 1] != t)
+				break;
+			
+			right++;
+		}
+		
+	}
+
+	void find(vector<int>& nums, int start, int end) {
+		int midIndex = start + (end - start) / 2;
+		int midVal = nums[midIndex];
+		
+		if(midVal == t) {
+			left = right = midIndex;
+			enlageToBoundries(nums);
+		} else if(midVal > t) {
+			if(midIndex != end)
+				find(nums, start, midIndex);
+		} else { //midVal < t
+			if(midIndex != start)
+				find(nums, midIndex, end);
+		}
+	}
+
+public:
+	vector<int> searchRange(vector<int>& nums, int target) {
+		vector<int> rval;
+		rval.resize(2, -1);
+
+		if(nums.size() == 0)
+			goto end;
+
+		if(nums.size() == 1) {
+			if(nums[0] == target)
+				rval[0] = rval[1] = 0;
+			
+			goto end;
+		}
+
+		t = target;
+		find(nums, 0, nums.size());
+		rval[0] = left;
+		rval[1] = right;
+
+	end:
+		return rval;
+	}
+};
